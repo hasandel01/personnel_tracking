4afbabf43babb452fbd50465d5c5ce4be29edb44
hasandel01 <hasan_deliktas01@outlook.com>
2024-07-23 16:46:58 +0300

    Initial commit.

---
+++ .pio/build/esp32dev/lib5ac/BLE/BLEExceptions.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/lib5ac/BLE/BLEExceptions.cpp.o: \
+ C:/Users/stajyer2/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
---
+++ .pio/build/esp32dev/src/employee.cpp.d
@@ -0,0 +1,1 @@
+.pio/build/esp32dev/src/employee.cpp.o: src/employee.cpp src/employee.h
